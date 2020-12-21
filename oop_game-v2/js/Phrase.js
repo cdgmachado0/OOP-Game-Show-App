2dde51d885b72ab4eeee3156ff413e7613b28aed
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2020-12-21 20:05:37 +0100

    Working on Meets

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -1,3 +1,30 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+ class Phrase {
+     constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+     }
+
+     addPhraseDisplay() {
+        const lettersOfPhrase = Array.from(this.phrase);
+        lettersOfPhrase.forEach(letter => {
+            const li = document.createElement('li');
+            li.textContent = letter;
+            if (letter !== '') {
+                li.className = `hide letter ${letter}`;
+            } else {
+                li.className = 'space'; //test this on app.js
+            }
+        });
+     }
+
+     checkLetter() {
+
+     }
+
+     showMatchedLetter() {
+
+     }
+ }
