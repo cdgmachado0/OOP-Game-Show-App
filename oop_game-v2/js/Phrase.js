ee95c55ccc376c682cf41167fa878e125b6e973c
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2020-12-22 00:05:00 +0100

    Still on Meets
    Deleted the for..loop for creating an array with all letters of the keyboard

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -8,23 +8,35 @@
      }
 
      addPhraseDisplay() {
+        const phraseUl = document.querySelector('#phrase').querySelector('ul');
         const lettersOfPhrase = Array.from(this.phrase);
         lettersOfPhrase.forEach(letter => {
             const li = document.createElement('li');
             li.textContent = letter;
-            if (letter !== '') {
+            if (letter !== ' ') {
                 li.className = `hide letter ${letter}`;
             } else {
-                li.className = 'space'; //test this on app.js
+                li.className = 'space';
+            }
+            phraseUl.appendChild(li);
+        });
+     }
+
+     checkLetter(userLetter) {
+        const lettersOfPhrase = Array.from(this.phrase);
+        lettersOfPhrase.forEach(letter => {
+            if (userLetter === letter) {
+                this.showMatchedLetter(userLetter);
             }
         });
      }
 
-     checkLetter() {
-
-     }
-
-     showMatchedLetter() {
-
+     showMatchedLetter(userLetter) {
+        const liLetters = document.querySelector('#phrase').querySelector('ul').children;
+        liLetters.forEach(li => {
+            if (li.textContent === userLetter) {
+                li.className = `show letter ${userLetter}`;
+            }
+        });
      }
  }
--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -1,3 +1,35 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
+
+const overlayDiv = document.querySelector('#overlay');
+const mainDivKeyboard = document.querySelector('#qwerty');
+const keyDivs = document.querySelectorAll('.keyrow');
+const startGameButton = document.querySelector('#btn__reset');
+
+startGameButton.addEventListener('click', e => {
+    overlayDiv.style.display = 'none';
+})
+
+
+// const lettersOfKeyboard = [];
+// for (let i = 0; i < keyDivs.length; i++) {
+//     let buttonsForLetters = keyDivs[i].children;
+//     for (let j = 0; j < buttonsForLetters.length; j++) {
+//         lettersOfKeyboard.push(buttonsForLetters[j].textContent);
+//     }
+// }
+
+
+// mainDivKeyboard.addEventListener('click', e => {
+//     if (e.target.tagName === 'BUTTON') {
+//         e.target.textContent
+//     }
+// });
+
+
+
+
+const newPhrase = new Phrase('Mucho gusto!');
+newPhrase.addPhraseDisplay();
+// newPhrase.checkLetter();
